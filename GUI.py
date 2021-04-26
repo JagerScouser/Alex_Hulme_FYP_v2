842ea549f44a758a55ca1c4373fadc84043274cf
JagerScouser <alexthulme@blueyonder.co.uk>
2021-04-26 18:50:36 +0100

    Added a userguide help button
    removed user guide help button
    added user help guide into the cascade drop down bar at the top
    removed redundant code and cleaned up parts
    
    26/04
    Read the UID from a dropdown menu
    Problem:
    It selects only 1 value rather than the selected value, regardless of which is selected.
    Attempted Tracing to allow it to work.
    TO DO:
    - Get code to split the dataset based on iPhoneUID

--- GUI.py
+++ GUI.py
@@ -2,15 +2,35 @@ import tkinter
 import tkinter.messagebox
 from tkinter import filedialog
 from tkinter import *
+import pandas as pd
+
 from Main import GPSVis
 import os
 
-
-def Open_Dataset():
+def Open_Dataset(Menu, dropVar, index=None):
     # This function will open file explorer
     # making the filename global allows for it to be called later down the line
     global data_set
     data_set = filedialog.askopenfilename()
+    global UID
+    UID=pd.read_csv(data_set)
+    UID=UID['iPhoneUID'].unique().tolist()
+    print(type(UID))
+    #for id in UID:
+        #print(id)
+    drop_menu = Menu["menu"]
+    #drop_menu.delete(0, "end")
+    for string in UID:
+        drop_menu.add_command(label=string,
+                              command= lambda value=string:
+                                dropVar.set(string))
+    print(dropVar)
+    for id in dropVar:
+       print(id)
+
+        #for loop, adds value to drop menu
+        #command is value = string
+
 def Open_Map():
     global Map
     Map = filedialog.askopenfilename()
@@ -18,6 +38,8 @@ def about_us():
     tkinter.messagebox.showinfo('About Us', 'This is a program made in Python by Alex Hulme for his Final Year Project')
 def help_guide():
     os.startfile(r"C:\Users\AlexT\OneDrive\Desktop\Python Project New\User_Guide.pdf")
+def option_changed(*args):
+    print("the user chose value {}".format(dropVar.get()))
 
 def GPS_Visualisation(entry1,entry2,entry3,entry4):
     vis = GPSVis(data_path=data_set,  # DATASET WITH COORDINATES
@@ -30,7 +52,7 @@ def GPS_Visualisation(entry1,entry2,entry3,entry4):
     # Sets the colour of the visualisation on map
     vis.plot_map(output='save')
 
-# Starts the tkinter GUI
+    # Starts the tkinter GUI
 root = Tk()
 
 # Creating a menu drop down bar
@@ -38,23 +60,29 @@ menubar = Menu(root)
 root.config(menu=menubar)
 # Creates the sub menu
 subMenu = Menu(menubar, tearoff=0)
-menubar.add_cascade(label="File",menu=subMenu)
+menubar.add_cascade(label="File", menu=subMenu)
 subMenu.add_command(label="New")
 subMenu.add_command(label="Exit", command=root.destroy)
 
 subMenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Help", menu=subMenu)
-subMenu.add_command(label="About us", command= about_us)
-subMenu.add_command(label="User Guide", command= help_guide)
+subMenu.add_command(label="About us", command=about_us)
+subMenu.add_command(label="User Guide", command=help_guide)
 
-
-root.title("PERSEC Application") # Titles the frame
-root.geometry('400x400') # Sets the Height and Width
-text = Label(root,text = 'Welcome to the Personal Security Application')
+root.title("PERSEC Application")
+# Titles the frame
+root.geometry('400x400')
+# Sets the Height and Width
+text = Label(root, text='Welcome to the Personal Security Application')
 text.pack()
-btn = Button(root, text="Select Dataset File", command = Open_Dataset)
+dropVar = tkinter.StringVar(root)
+dropVar.trace("w", option_changed)
+options_list = ["select a file"]
+drop_menu = tkinter.OptionMenu(root, dropVar, ())
+drop_menu.pack()
+btn = Button(root, text="Select Dataset File", command=lambda: Open_Dataset(drop_menu, dropVar))
 btn.pack()
-btn2 = Button(root, text="Select Map File", command = Open_Map)
+btn2 = Button(root, text="Select Map File", command=Open_Map)
 btn2.pack()
 
 # Allows user to input Map latitude and longitude
@@ -82,7 +110,8 @@ entry4.pack()
 
 # Add an error check later (int check, prescence check)
 # select present --- Dialog box
-submit = Button(root, text="Submit", command = lambda: GPS_Visualisation(entry1.get(), entry2.get(), entry3.get(), entry4.get()))
+submit = Button(root, text="Submit",
+                command=lambda: GPS_Visualisation(entry1.get(), entry2.get(), entry3.get(), entry4.get()))
 submit.pack()
 
 
